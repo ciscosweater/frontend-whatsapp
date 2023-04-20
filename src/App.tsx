d09fb27b4c2ee67b81d45992ba9515386593b132
Gustavo Francisco <ciscosweater@gmail.com>
2023-04-20 15:30:48 -0300

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
-import { Background, ChatContainer, ChatItem, ChatOptions, ContactsList, Container, ImageProfile, LastMessage, TitleChatContainer, TitleMessage } from './styles';
-import Image from './assets/profissao-programador.jpg'
+import React, {useEffect, useState} from 'react';
+import { Background, CenterMessageContainer, ChatContainer, ChatHeader, ChatInput, ChatInputArea, ChatItem, ChatMessages, ChatMessagesArea, ChatOptions, ContactsList, Container, CurrentChat, CurrentChatContainer, CurrentChatInfo, CurrentChatTitle, ImageProfile, LastMessage, LeftMessageContainer, NeutralMessage, ReceivedMessage, RightMessageContainer, SendMessageIcon, SendedMessage, TitleChatContainer, TitleMessage } from './styles';
+import GroupImage from './assets/profissao-programador.jpg';
+import SendIcon from './assets/send.png';
+import socket from 'socket.io-client';
+
+const io = socket('http://localhost:4000');
 
 function App() {
+
+  const [name, setName] = useState<string>('');
+  const [joined, setJoined] = useState<boolean>(false);
+  const [usersList, setUsersList] = useState([]);
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<string[]>([]);
+  const [lastMessage, setLastMessage] = useState<any>({});
+  
+  useEffect(() => {
+    io.on("usersUpdate", (users) => setUsersList(users));
+    io.on("joinMessage", (joinMessageToAdd: string) => setMessages((messages) => [...messages, joinMessageToAdd]));
+    io.on("updateMessages", (messageToAdd: string) => setMessages((messages) => [...messages, messageToAdd]));
+  }, []);
+
+  useEffect(() => {
+    messages.length? setLastMessage(messages[messages.length - 1]) : setLastMessage({});
+  }, [messages])
+
+  const handleJoin = () => {
+    if(name){
+      io.emit("userJoin", name);
+      setJoined(true);
+    }
+  };
+
+  const handleMessage = () => {
+    if(message){
+      io.emit("sendMessage", {message, name});
+      setMessage('');
+    };
+  };
+
+  if(!joined){
+    return (
+      <div>
+        <span>Digite seu nome</span>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={() => handleJoin()} >Entrar</button>
+      </div>
+    );
+  };
+
   return (
     <Container>
       <Background></Background>
@@ -10,13 +56,74 @@ function App() {
           <ContactsList>
             <ChatOptions></ChatOptions>
             <ChatItem>
-              <ImageProfile src={Image} alt="" />
+              <ImageProfile src={GroupImage} alt="" />
               <TitleChatContainer>
                 <TitleMessage>Networking Profissão Programador</TitleMessage>
-                <LastMessage>Cisco: Bom dia!</LastMessage>
+                <LastMessage>
+                  {
+                  `${lastMessage.name? `${lastMessage.name}: ` : ''}${lastMessage.message}`
+                  }
+                </LastMessage>
               </TitleChatContainer>
             </ChatItem>
           </ContactsList>
+          <ChatMessages>
+            <ChatHeader>
+              <CurrentChat>
+                <ImageProfile src={GroupImage} alt="" />
+                <CurrentChatContainer>
+                  <CurrentChatTitle>Networking Profissão Programador</CurrentChatTitle>
+                  <CurrentChatInfo>
+                    {usersList.map((user: any, index) => (
+                      <span>{user.name}{index + 1 < usersList.length? ', ' : ''}</span>
+                    ))}
+                    </CurrentChatInfo>
+                </CurrentChatContainer>
+              </CurrentChat>
+            </ChatHeader>
+
+            <ChatMessagesArea>
+              {messages.map((message:any, index) => {
+                if(message.name === name){
+                  return (
+                    <RightMessageContainer>
+                      <SendedMessage>
+                        <span key={index} >{message.name? `${message.name}: ` : ''}{message.message}</span>
+                      </SendedMessage>
+                    </RightMessageContainer>
+                  )
+                } else if(message.name === null){
+                  return (
+                  <CenterMessageContainer>
+                    <NeutralMessage>
+                      <span key={index} >{message.name? `${message.name}: ` : ''}{message.message}</span>
+                    </NeutralMessage>
+                  </CenterMessageContainer>
+                  )
+                } else {
+                  return (
+                    <LeftMessageContainer>
+                      <ReceivedMessage>
+                        <span key={index} >{message.name? `${message.name}: ` : ''}{message.message}</span>
+                      </ReceivedMessage>
+                    </LeftMessageContainer>
+                  )
+                
+                }})}
+            </ChatMessagesArea>
+
+            <ChatInputArea>
+              <ChatInput 
+              placeholder='Mensagem' 
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              />
+              <SendMessageIcon 
+              src={SendIcon} 
+              onClick={() => handleMessage()}
+              />
+            </ChatInputArea>
+          </ChatMessages>
       </ChatContainer>
     </Container>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BackgroundImage from './assets/background.png'
 
 export const Container = styled.div`
     height: 100vh;
@@ -36,9 +37,33 @@ export const ChatOptions = styled.div`
     width: 100%;
     height: 80px;
     background-color: #f0f2f5;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 0 10px;
+`;
+
+export const ChatHeader = styled.div`
+    width: 100%;
+    height: 80px;
+    background-color: #f0f2f5;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 0 10px;
 `;
 
 export const ChatItem = styled.div`
+    box-sizing: border-box;
+    padding: 20px;
+    border-top: 1px solid #f0f2f5;
+    border-bottom: 1px solid #f0f2f5;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+`;
+
+export const CurrentChat = styled.div`
     box-sizing: border-box;
     padding: 10px;
     border-top: 1px solid #f0f2f5;
@@ -49,7 +74,7 @@ export const ChatItem = styled.div`
 `;
 
 export const ImageProfile = styled.img`
-    width: 60px;
+    width: 50px;
     height: auto;
     border-radius: 50%;
 `;
@@ -60,10 +85,115 @@ export const TitleChatContainer = styled.div`
     margin-left: 15px;
 `;
 
+export const CurrentChatContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+`;
+
 export const TitleMessage = styled.span`
+    font-size: 18px;
+`;
+
+export const CurrentChatTitle = styled.span`
     font-size: 18px;
 `;
 
 export const LastMessage = styled.span`
     color: #808080;
+`;
+
+export const CurrentChatInfo = styled.span`
+    color: #808080;
+`;
+
+export const ChatMessages = styled.div`
+    flex: 1;
+    background-image: url(${BackgroundImage});
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export const ChatMessagesArea = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    max-height: 100%;
+    overflow-y: auto;
+`;
+
+export const ChatInputArea = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: #f0f2f5;
+    box-sizing: border-box;
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const ChatInput = styled.input`
+    width: 90%;
+    background-color: #fff;
+    height: 45px;
+    border: none;
+    outline: none;
+    border-radius: 8px;
+    box-sizing: border-box;
+    padding: 4px 20px;
+    font-size: 16px;
+`;
+
+export const SendMessageIcon = styled.img`
+    width: 30px;
+    height: auto;
+    cursor: pointer;
+`;
+
+export const RightMessageContainer = styled.div`
+    width: 100%;
+    display: flex;
+    box-sizing: border-box;
+    padding: 0 20px;
+    margin: 2px 0;
+    justify-content: flex-end;
+`;
+
+export const LeftMessageContainer = styled.div`
+    width: 100%;
+    display: flex;
+    box-sizing: border-box;
+    padding: 0 20px;
+    margin: 2px 0;
+    justify-content: flex-start;
+`;
+
+export const CenterMessageContainer = styled.div`
+    width: 100%;
+    display: flex;
+    box-sizing: border-box;
+    padding: 0 20px;
+    margin: 2px 0;
+    justify-content: center;
+`;
+
+export const SendedMessage = styled.div`
+    background-color: #d9fdd3;
+    padding: 10px;
+    border-radius: 5px;
+`;
+
+export const ReceivedMessage = styled.div`
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+`;
+
+export const NeutralMessage = styled.div`
+    background-color: #dacdbe;
+    border: .5px solid #c8b49d;
+    padding: 10px;
+    border-radius: 5px;
 `;
